5601b6ba4ab9851cfe4d2e363670b5b3f00fd048
Paras Mogla <parasmogla01644@gmail.com>
2022-12-01 18:37:27 +0530

    selected tab name updated in footer

--- src/app/shared/product-price-info/product-price-info.component.ts
+++ src/app/shared/product-price-info/product-price-info.component.ts
@@ -1,18 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-price-info',
   templateUrl: './product-price-info.component.html',
-  styleUrls: ['./product-price-info.component.scss']
+  styleUrls: ['./product-price-info.component.scss'],
 })
 export class ProductPriceInfoComponent implements OnInit {
+  @Input() description: boolean = false;
+  @Input() usp: boolean = false;
+  @Input() social: boolean = false;
+  @Input() accept: boolean = false;
+  @Input() isDisable: boolean = true;
 
-  constructor() { }
-  @Input() description:boolean=false;  
-  @Input() usp:boolean=false;  
-  @Input() social:boolean=false;  
-  @Input() accept:boolean=false;  
-  ngOnInit(): void {
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {}
+
+  gotoCheckout() {
+    this.router.navigate(['checkout']);
   }
-
 }
