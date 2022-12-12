4e56a34213f21aa5afa0c7851026d403f34631b4
Paras Mogla <parasmogla01644@gmail.com>
2022-12-12 16:18:04 +0530

    modal class on body when modal open

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,21 +1,42 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { addClassToBody, removeClassToBody } from 'src/app/utils/utils';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnChanges, OnDestroy {
   @Output() close: EventEmitter<void> = new EventEmitter();
-   @Input() headerTitle: string = ''; 
-   @Input() headerCustomClass !: string[];
-   @Input() activeClass: boolean = false; 
+  @Input() headerTitle: string = '';
+  @Input() headerCustomClass!: string[];
+  @Input() activeClass: boolean = false;
 
   constructor() {}
+
+  ngOnChanges() {
+    if (this.activeClass) {
+      addClassToBody('is-modal');
+    } else {
+      removeClassToBody('is-modal');
+    }
+  }
 
   ngOnInit(): void {}
 
   closeModal() {
     this.close.emit();
+  }
+
+  ngOnDestroy() {
+    removeClassToBody('is-modal');
   }
 }
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,7 +1,15 @@
-import { findFlagUrlByIso2Code } from 'country-flags-svg';
-export function countryFlag(isoCode: string) {
-  if (!isoCode?.length) {
-    return;
-  }
-  return findFlagUrlByIso2Code(isoCode);
+// import { findFlagUrlByIso2Code } from 'country-flags-svg';
+// export function countryFlag(isoCode: string) {
+//   if (!isoCode?.length) {
+//     return;
+//   }
+//   return findFlagUrlByIso2Code(isoCode);
+// }
+
+export function addClassToBody(className: string) {
+  document.body.classList.add(className);
+}
+
+export function removeClassToBody(className: string) {
+  document.body.classList.remove(className);
 }
