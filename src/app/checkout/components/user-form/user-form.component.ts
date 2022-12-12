8cc5636c7b7725385000d90571515deaf543355d
Paras Mogla <parasmogla01644@gmail.com>
2022-12-12 17:56:26 +0530

    comment removed

--- src/app/checkout/components/user-form/user-form.component.ts
+++ src/app/checkout/components/user-form/user-form.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import {
-//   noWhitespaceValidator,
-//   RegularExpressionConstant,
-// } from '@vaildators/validators';
-
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
@@ -12,31 +7,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class UserFormComponent implements OnInit {
   @Input() groupName!: FormGroup;
-  // userForm!: FormGroup;
 
-  // constructor(private _fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    // this.initForm();
-  }
-
-  // initForm() {
-  //   this.userForm = this._fb.group({
-  //     firstName: ['', [Validators.required, noWhitespaceValidator]],
-  //     middleName: [''],
-  //     lastName: ['', [Validators.required]],
-  //     email: [
-  //       '',
-  //       [
-  //         Validators.required,
-  //         Validators.pattern(RegularExpressionConstant?.EMAIL),
-  //       ],
-  //     ],
-  //     phone: ['', [Validators.required]],
-  //     postCode: ['', [Validators.required]],
-  //     houseNumber: [''],
-  //     streetName: [''],
-  //     landMark: ['', [Validators.required]],
-  //   });
-  // }
+  ngOnInit(): void {}
 }
