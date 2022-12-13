75afac8f7c404a5101d48f08ac723635249652d3
Paras Mogla <parasmogla01644@gmail.com>
2022-12-13 13:15:01 +0530

    back button text updated

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export const BackButtonText = {
+  checkout: 'Back to Overview',
+  default: 'Terug naar configuratie',
+};
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  buttonText: string = BackButtonText.default;
   constructor(private readonly router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.buttonText =
+      this.router.url === '/checkout'
+        ? BackButtonText.checkout
+        : BackButtonText.default;
+  }
 
   gotoConfigurationPage() {
     const currentRoute: string = this.router.url;
